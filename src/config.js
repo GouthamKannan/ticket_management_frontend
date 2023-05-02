b1ad05500344a653daaa508dad52c0b8d954e2aa
Goutham Kannan <54542723+GouthamKannan@users.noreply.github.com>
2023-05-02 15:39:01 +0530

    Update backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const configs = {
-    api_url : "https://ticket-management-backend-test.herokuapp.com"
+    // api_url : "https://ticket-management-backend-test.herokuapp.com"
+    api_url : "https://ticket-management-backend-test.onrender.com"
 }
 
 export default configs
